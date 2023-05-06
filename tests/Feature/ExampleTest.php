eebc6122a6804b1bd88905ff2aaecbbf5aa09b07
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2023-05-06 16:56:57 +0200

    start testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,12 +7,20 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
+
+        $response->assertSee('Documentation'); // Assert that the request containes some text
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_homepage_contains_symfony(): void
+    {
+        $response = $this->get('/');
+
+        //$response-> assertSee('Symfony'); // Will return error as it dose not containe this text
 
         $response->assertStatus(200);
     }
