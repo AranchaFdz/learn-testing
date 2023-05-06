a761f8b8bc6d92c36260338bfa1b62d222594ea0
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2023-05-06 17:26:39 +0200

    6 basic tests passed

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,7 +7,14 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_application_returns_a_successful_response()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_homepage_contains_text()
     {
         $response = $this->get('/');
 
@@ -16,11 +23,38 @@ class ExampleTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_the_homepage_contains_symfony(): void
+    public function test_the_homepage_contains_text_in_order()
+    {
+        $response = $this->get('/');
+
+        $response->assertSeeTextInOrder(['Documentation', 'Laracast']); // Assert that the request containes text in order
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_homepage_contains_symfony()
     {
         $response = $this->get('/');
 
         //$response-> assertSee('Symfony'); // Will return error as it dose not containe this text
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_homepage_dose_not_contain_symfony()
+    {
+        $response = $this->get('/');
+
+        $response-> assertDontSee('Symfony'); 
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_about_route_returns_a_successful_respone()
+    {
+        $response = $this->get('/about');
+
+        $response->assertSee('About');
 
         $response->assertStatus(200);
     }
